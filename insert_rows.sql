c555dafc941015f937f2500006b94d2687a5749a
Kang MinSeok <stillnessfate@gmail.com>
2018-12-11 02:52:31 +0900

    Create insert_rows.sql

---
+++ insert_rows.sql
@@ -0,0 +1,5 @@
+INSERT INTO room_type ( name, capacity, smoking, price) VALUES ('Premium', 2, 0, 180000);
+INSERT INTO room_type ( name, capacity, smoking, price) VALUES ('Suite', 2, 0, 150000);
+INSERT INTO room_type ( name, capacity, smoking, price) VALUES ('Single', 1, 1, 80000);
+INSERT INTO room_type ( name, capacity, smoking, price) VALUES ('Twin', 2, 0, 110000);
+INSERT INTO room_type ( name, capacity, smoking, price) VALUES ('Triple', 3, 0, 140000);
