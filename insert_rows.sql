f833f29950c2555fc6e31c85401e7b02ef2db80b
joyon1104 <37361830+joyon1104@users.noreply.github.com>
2018-12-11 14:07:25 +0900

    Update insert_rows.sql

--- insert_rows.sql
+++ insert_rows.sql
@@ -3,3 +3,7 @@ INSERT INTO room_type ( name, capacity, smoking, price) VALUES ('Suite', 2, 0, 1
 INSERT INTO room_type ( name, capacity, smoking, price) VALUES ('Single', 1, 1, 80000);
 INSERT INTO room_type ( name, capacity, smoking, price) VALUES ('Twin', 2, 0, 110000);
 INSERT INTO room_type ( name, capacity, smoking, price) VALUES ('Triple', 3, 0, 140000);
+
+INSERT INTO payment_type (name) values ('신용카드');
+INSERT INTO payment_type (name) values ('계좌이체');
+INSERT INTO payment_type (name) values ('무통장입금');
